ac295b4067616e757fd473c4e8a908b584f7c625
zhukovv <northbug@yandex.ru>
2022-08-09 15:37:29 +0500

    первая задача полностью

---
+++ example1/Program.cs
@@ -0,0 +1,15 @@
+﻿int max = 0, min =0;
+int value1 = Convert.ToInt32(Console. ReadLine());
+int value2 = Convert.ToInt32(Console .ReadLine());
+if(value1 > value2) 
+{
+max = value1;
+min = value2;
+}
+else
+{
+max = value2;
+min = value1;   
+}
+
+ Console .WriteLine($"Максимальное число {max}  минимальное {min} ");
